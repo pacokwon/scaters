c369aa9fbed4fdf985581e0e30c241e162121295
pacokwon <haechank@gmail.com>
2021-09-14 19:28:05 +0900

    feat: accept rom file from command line

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,21 @@ mod font;
 mod gfx;
 mod lib;
 
+use std::env;
 use emu::Emulator;
 
 fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: ./remu <path-to-rom>");
+        std::process::exit(1);
+    }
+
+    let rom_file = &args[1];
+
     let mut emu = Emulator::new()?;
-    emu.load_rom("roms/pong.ch8");
+    emu.load_rom(rom_file);
     emu.run_loop()?;
     Ok(())
 }
